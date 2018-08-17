aa278ff9f2fb37757148b209fb7b32e99962196f
Abhishek <abhicena8@gmail.com>
2018-08-18 00:23:52 +0530

    acad

---
+++ assignment 2.py
@@ -0,0 +1,24 @@
+#ques1
+print('I am skrillex')
+#ques2
+print('chit'+'kara')
+#ques3
+x=int(input('Enter the value of x\n'))
+y=int(input('Enter the value of y\n'))
+z=int(input('Enter the value of z\n'))
+print(x)
+print(y)
+print(z)
+#ques4
+print("Let's get started")
+#ques5
+s="Acadview"
+print(s)
+course="Python"
+print(course)
+fees="5000"
+print(fees)
+#ques6
+r = int(input("Enter a number: "))
+area = 3.14*r*r
+print(area)
